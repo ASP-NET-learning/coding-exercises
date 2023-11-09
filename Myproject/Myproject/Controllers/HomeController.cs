596fe78b17c91e880c6e1488271ff1dffb4e9a99
klas9802 <klas9806@gmail.com>
2023-11-09 17:45:39 +0800

    first commit

---
+++ Myproject/Myproject/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Myproject.Models;
+using System.Diagnostics;
+
+namespace Myproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserPermissions()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ProductionOrders()
+        {
+            return View();
+        }
+
+        public IActionResult ResourceMonitoring()
+        {
+            return View();
+        }
+
+        public IActionResult ProductionData()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 練習MVC/練習MVC/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using 練習MVC.Models;
+
+namespace 練習MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Production_Orders()
+        {
+            return View();
+        }
+
+        public IActionResult Resource_Monitoring()
+        {
+            return View();
+        }
+
+        public IActionResult Production_Data()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
