cc4050918a2858f436a1f0797d0abb2d2e8abebd
klas9802 <klas9806@gmail.com>
2023-11-25 09:34:53 +0800

    Success build openapi

---
+++ MvcOpenApi/Controllers/UvaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcOpenApi.Models;
+using Newtonsoft.Json;
+
+
+namespace MvcOpenApi.Controllers
+{
+    public class UvaController : Controller
+    {
+        public async Task<ActionResult> UvaPoint()
+        {
+            string targetURL = "https://data.moenv.gov.tw/api/v2/uv_s_01?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON";
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(targetURL);
+            var JsonResponse = JsonConvert.DeserializeObject<Rootobject>(response);
+            //ViewBag.response = response;
+            return View(JsonResponse.records);
+        }
+    }
+}
+
+
---
+++ MvcOpenApi/Models/Uva.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcOpenApi.Models
+{
+    public class Rootobject
+    {
+        public Field[] fields { get; set; }
+        public string resource_id { get; set; }
+        public __Extras __extras { get; set; }
+        public bool include_total { get; set; }
+        public string total { get; set; }
+        public string resource_format { get; set; }
+        public string limit { get; set; }
+        public string offset { get; set; }
+        public _Links _links { get; set; }
+        public Record[] records { get; set; }
+    }
+
+    public class __Extras
+    {
+        public string api_key { get; set; }
+    }
+
+    public class _Links
+    {
+        public string start { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Field
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public Info info { get; set; }
+    }
+
+    public class Info
+    {
+        public string label { get; set; }
+    }
+
+    public class Record
+    {
+        [Display(Name = "測站名稱")]
+        public string sitename { get; set; }
+        [Display(Name = "紫外線指數")]
+        public string uvi { get; set; }
+        [Display(Name = "發布單位")]
+        public string unit { get; set; }
+        [Display(Name = "縣市")]
+        public string county { get; set; }
+        [Display(Name = "WGS84經度")]
+        public string wgs84_lon { get; set; }
+        [Display(Name = "WGS84緯度")]
+        public string wgs84_lat { get; set; }
+        [Display(Name = "資料發佈時間")]
+        public string datacreationdate { get; set; }
+    }
+
+
+}
